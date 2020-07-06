d52d5865483a406154321dd871927b4860d14962
Kstan <standragos82@gmail.com>
2020-07-06 20:13:39 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+class Animal:
+
+    def __init__(name, type, breed, age):
+        self.name = name
+        self.type = type
+        self.breed = breed
+        self.age = age
+
+name = input('Animal name')
+type = input('Animal type')
+bree = input('Animal breed')
+age = input('Animal age')
+
+animal1 = Animal(name, type, breed, age)
+
+print('Animal {name}, type {type}, breed {breed} and age of {age} was added'.format(name, type, breed, age))
