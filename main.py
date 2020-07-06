12a605b3f5f49d93ee6864692595b234da22f626
Kstan <standragos82@gmail.com>
2020-07-06 20:55:40 +0300

    working class

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 
 class Animal:
 
-    def __init__(name, type, breed, age):
+    def __init__(self, name, type, breed, age):
         self.name = name
         self.type = type
         self.breed = breed
         self.age = age
 
-name = input('Animal name')
-type = input('Animal type')
-breed = input('Animal breed')
-age = input('Animal age')
+name = input('Animal name: ')
+type = input('Animal type: ')
+breed = input('Animal breed: ')
+age = input('Animal age: ')
 
 animal1 = Animal(name, type, breed, age)
 
-print('Animal {name}, type {type}, breed {breed} and age of {age} was added'.format(name, type, breed, age))
+print('Animal {name}, type {type}, breed {breed} and age of {age} was added'.format(name = name, type = type, breed = breed, age = age))
+
+print(animal1.age)
